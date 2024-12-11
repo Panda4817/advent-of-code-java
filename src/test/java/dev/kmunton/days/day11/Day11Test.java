ba8c765a4e992d478c99843f9b1c2443f89a364f
Kanta Munton <kanta.munton@gmail.com>
2024-12-11 09:18:40 +0000

    Complete day 11

---
+++ src/test/java/dev/kmunton/days/day11/Day11Test.java
@@ -0,0 +1,19 @@
+package dev.kmunton.days.day11;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class Day11Test {
+
+  private static Day11 day;
+
+  @Test
+  void part1_success() {
+    List<String> input = """
+        125 17""".lines().toList();
+    day = new Day11(input);
+    assertEquals(55312L, day.part1());
+  }
+}
