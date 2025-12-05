e2e0f93d80eb243a6c9030cf601d720504f87e96
Kanta Munton <kanta.munton@gmail.com>
2025-12-05 08:13:45 +0000

    Complete 2025 day 5

--- src/main/java/dev/kmunton/year2025/day5/Day5.java
+++ src/main/java/dev/kmunton/year2025/day5/Day5.java
@@ -1,25 +1,87 @@
 package dev.kmunton.year2025.day5;
 
 import dev.kmunton.utils.days.Day;
+import dev.kmunton.utils.geometry.Range;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 public class Day5 implements Day<Long, Long> {
+
+    private final Set<Range> idRanges = new HashSet<>();
+    private final List<Long> ids = new ArrayList<>();
 
   public Day5(List<String> input) {
     processData(input);
   }
 
   @Override
+  public void processData(List<String> input) {
+      boolean atIdList = false;
+      for  (String line : input) {
+          if (line.isEmpty()) {
+              atIdList = true;
+              continue;
+          }
+          if (atIdList) {
+              ids.add(Long.parseLong(line));
+              continue;
+          }
+          final String[] split = line.split("-");
+          final Range range = new Range(Long.parseLong(split[0]), Long.parseLong(split[1]));
+          idRanges.add(range);
+      }
+  }
+
+  @Override
   public Long part1() {
-    return -1L;
+      long freshIds = 0;
+      for  (final Long id : ids) {
+          if (isFresh(id)) {
+              freshIds++;
+          }
+      }
+    return freshIds;
+  }
+
+  private boolean isFresh(final long id) {
+      for  (final Range range : idRanges) {
+          if (range.contains(id)) {
+            return true;
+          }
+      }
+      return false;
   }
 
   @Override
   public Long part2() {
-    return -1L;
+      long freshIds = 0;
+      final Set<Range> rangesDone = new HashSet<>();
+      for (final Range range : idRanges) {
+          if (rangesDone.contains(range)) {
+              continue;
+          }
+          rangesDone.add(range);
+          Range unionedRange = range;
+          boolean foundOverlap = false;
+          do {
+              foundOverlap = false;
+              for (final Range secondRange : idRanges) {
+                  if (rangesDone.contains(secondRange)) {
+                      continue;
+                  }
+                  if (unionedRange.overlaps(secondRange)) {
+                      unionedRange = unionedRange.union(secondRange);
+                      rangesDone.add(secondRange);
+                      foundOverlap = true;
+                  }
+              }
+          } while (foundOverlap);
+          freshIds += (unionedRange.length() + 1);
+      }
+
+    return freshIds;
   }
 
 }
--- src/test/java/dev/kmunton/year2025/day5/Day5Test.java
+++ src/test/java/dev/kmunton/year2025/day5/Day5Test.java
@@ -1,5 +1,6 @@
 package dev.kmunton.year2025.day5;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,19 +11,30 @@ class Day5Test {
 
   private static Day5 day;
 
+  @BeforeEach
+  void setUp() {
+      List<String> input = """
+            3-5
+            10-14
+            16-20
+            12-18
+            
+            1
+            5
+            8
+            11
+            17
+            32""".lines().toList();
+      day = new Day5(input);
+  }
+
   @Test
   void part1_success() {
-    List<String> input = """
-        fill with test data""".lines().toList();
-    day = new Day5(input);
-    assertEquals(-1L, day.part1());
+    assertEquals(3L, day.part1());
   }
 
   @Test
   void part2_success() {
-    List<String> input = """
-        fill with test data""".lines().toList();
-    day = new Day5(input);
-    assertEquals(-1L, day.part2());
+    assertEquals(14L, day.part2());
   }
 }
